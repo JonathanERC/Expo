c57cacb65bbcd7a094b3fa7f03067ab204550ef0
JonathanERC <jonathanestebanrondon@gmail.com>
2018-11-21 17:19:31 -0400

    Archivos de la expo
    exp

---
+++ Expo/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Expo
+{
+    partial class Expo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Expo));
+            this.btnnullables = new System.Windows.Forms.Button();
+            this.btntiposgenericos = new System.Windows.Forms.Button();
+            this.btncoleccionesgenericas = new System.Windows.Forms.Button();
+            this.btnmanejodeexcepciones = new System.Windows.Forms.Button();
+            this.btnlinq = new System.Windows.Forms.Button();
+            this.btnxml = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnnullables
+            // 
+            this.btnnullables.Location = new System.Drawing.Point(12, 12);
+            this.btnnullables.Name = "btnnullables";
+            this.btnnullables.Size = new System.Drawing.Size(79, 60);
+            this.btnnullables.TabIndex = 0;
+            this.btnnullables.Text = "Tipos Nullables";
+            this.btnnullables.UseVisualStyleBackColor = true;
+            this.btnnullables.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btntiposgenericos
+            // 
+            this.btntiposgenericos.Location = new System.Drawing.Point(97, 12);
+            this.btntiposgenericos.Name = "btntiposgenericos";
+            this.btntiposgenericos.Size = new System.Drawing.Size(79, 60);
+            this.btntiposgenericos.TabIndex = 1;
+            this.btntiposgenericos.Text = "Tipos Genericos";
+            this.btntiposgenericos.UseVisualStyleBackColor = true;
+            this.btntiposgenericos.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btncoleccionesgenericas
+            // 
+            this.btncoleccionesgenericas.Location = new System.Drawing.Point(12, 78);
+            this.btncoleccionesgenericas.Name = "btncoleccionesgenericas";
+            this.btncoleccionesgenericas.Size = new System.Drawing.Size(79, 60);
+            this.btncoleccionesgenericas.TabIndex = 1;
+            this.btncoleccionesgenericas.Text = "Colecciones Genericos";
+            this.btncoleccionesgenericas.UseVisualStyleBackColor = true;
+            this.btncoleccionesgenericas.Click += new System.EventHandler(this.btncoleccionesgenericas_Click);
+            // 
+            // btnmanejodeexcepciones
+            // 
+            this.btnmanejodeexcepciones.Location = new System.Drawing.Point(97, 78);
+            this.btnmanejodeexcepciones.Name = "btnmanejodeexcepciones";
+            this.btnmanejodeexcepciones.Size = new System.Drawing.Size(79, 60);
+            this.btnmanejodeexcepciones.TabIndex = 1;
+            this.btnmanejodeexcepciones.Text = "Manejo de Excepciones";
+            this.btnmanejodeexcepciones.UseVisualStyleBackColor = true;
+            this.btnmanejodeexcepciones.Click += new System.EventHandler(this.btnmanejodeexcepciones_Click);
+            // 
+            // btnlinq
+            // 
+            this.btnlinq.Location = new System.Drawing.Point(182, 12);
+            this.btnlinq.Name = "btnlinq";
+            this.btnlinq.Size = new System.Drawing.Size(79, 60);
+            this.btnlinq.TabIndex = 2;
+            this.btnlinq.Text = "Linq";
+            this.btnlinq.UseVisualStyleBackColor = true;
+            this.btnlinq.Click += new System.EventHandler(this.btnlinq_Click);
+            // 
+            // btnxml
+            // 
+            this.btnxml.Location = new System.Drawing.Point(182, 78);
+            this.btnxml.Name = "btnxml";
+            this.btnxml.Size = new System.Drawing.Size(79, 60);
+            this.btnxml.TabIndex = 3;
+            this.btnxml.Text = "XML";
+            this.btnxml.UseVisualStyleBackColor = true;
+            this.btnxml.Click += new System.EventHandler(this.btnxml_Click);
+            // 
+            // Expo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 148);
+            this.Controls.Add(this.btnxml);
+            this.Controls.Add(this.btnlinq);
+            this.Controls.Add(this.btnmanejodeexcepciones);
+            this.Controls.Add(this.btncoleccionesgenericas);
+            this.Controls.Add(this.btntiposgenericos);
+            this.Controls.Add(this.btnnullables);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Expo";
+            this.Text = "Expo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnnullables;
+        private System.Windows.Forms.Button btntiposgenericos;
+        private System.Windows.Forms.Button btncoleccionesgenericas;
+        private System.Windows.Forms.Button btnmanejodeexcepciones;
+        private System.Windows.Forms.Button btnlinq;
+        private System.Windows.Forms.Button btnxml;
+    }
+}
+
---
+++ Expo/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Expo
+{
+    public partial class Expo : Form
+    {
+        public Expo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int? enteronullable = null;
+            if (enteronullable.HasValue)
+            {
+                Console.WriteLine("La caja está llena");
+            }
+            else
+            {
+                Console.WriteLine("La caja está vacía");
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Lista de tipo generico
+            List<int> ListGeneric = new List<int> { 5, 9, 1, 4 };
+            //Lista de tipo no generico
+            ArrayList ListNonGeneric = new ArrayList { 5, 9, 1, 4 };
+            //Temporizador de sorteo generico
+            Stopwatch s = Stopwatch.StartNew();
+            ListGeneric.Sort();
+            s.Stop();
+            Console.WriteLine($"Sorteo no generico: {ListGeneric}  \n Tiempo tomado: {s.Elapsed.TotalMilliseconds}ms");
+
+            //Temporizador de sorteo no generico
+            Stopwatch s2 = Stopwatch.StartNew();
+            ListNonGeneric.Sort();
+            s2.Stop();
+            Console.WriteLine($"Sorteo no generico: {ListNonGeneric}  \n Tiempo tomado: {s2.Elapsed.TotalMilliseconds}ms");
+            Console.ReadLine();
+        }
+
+        private void btncoleccionesgenericas_Click(object sender, EventArgs e)
+        {
+            string linea;
+            List<string> _listadefrutas = new List<string>();
+            _listadefrutas.Add("Manzana");
+            _listadefrutas.Add("Naranja");
+            _listadefrutas.Add("Pera");
+            List<string> _listadeverduras = new List<string>(_listadefrutas);
+            _listadeverduras.Add("Papa");
+            _listadeverduras.Add("Yautia");
+            Console.WriteLine("Lista Normal");
+            Console.WriteLine("--------------------------------");
+            foreach (string item in _listadeverduras)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Buscar en una lista");
+            Console.Write("Inserte el articulo a buscar: ");
+            linea = Console.ReadLine();
+            Console.WriteLine("--------------------------------");
+            if (_listadeverduras.Contains(linea))
+            {
+                Console.WriteLine(linea+" encontrada");
+            }
+            else
+            {
+                Console.WriteLine("No existe el articulo buscado");
+            }
+        }
+
+        private void btnmanejodeexcepciones_Click(object sender, EventArgs e)
+        {
+            int num1, num2, resultado;
+            string linea;
+            Console.Write("Ingrese primer valor: ");
+            linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+            Console.Write("Ingrese segundo valor: ");
+            linea = Console.ReadLine();
+            num2 = int.Parse(linea);
+            try
+            {
+                Console.Write("El resultado de la division es: ");
+                resultado = num1 / num2;
+                Console.WriteLine(resultado);
+            }
+            catch (DivideByZeroException)
+            {
+
+                Console.WriteLine("No se puede dividir entre cero");
+            }
+        }
+
+        private void btnlinq_Click(object sender, EventArgs e)
+        {
+            List<int> numeros = new List<int>()
+            {
+                3, 5, 7, 8, 2, -3, -100, 526, 6, 22
+            };
+            var resultado = numeros.Where(x => x > 0).ToList();
+            //Poner debug en console.read() y poner el mouse encima de resultado
+            Console.Read();
+        }
+
+        private void btnxml_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlElement raiz = doc.CreateElement("Libros");
+            doc.AppendChild(raiz);
+
+            XmlElement libro = doc.CreateElement("libro");
+            raiz.AppendChild(libro);
+
+            XmlElement titulo = doc.CreateElement("titulo");
+            titulo.AppendChild(doc.CreateTextNode("El Hobbit"));
+            libro.AppendChild(titulo);
+
+            XmlElement precio = doc.CreateElement("precio");
+            precio.AppendChild(doc.CreateTextNode("RD$ 100"));
+            libro.AppendChild(precio);
+
+            libro = doc.CreateElement("libro");
+            raiz.AppendChild(libro);
+
+            doc.Save(@"c:\libro.xml");
+            //cambiar por el desktop
+        }
+    }
+}
